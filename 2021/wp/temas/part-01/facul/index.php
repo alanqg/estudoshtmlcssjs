7feff1738de2170d7c185acc9ba0b38bc7b8368b
Alan Queirós de Goes <alanqg@gmail.com>
2022-02-02 18:11:12 -0300

    Primeiros Passos Criando Tema
    Tema replicado das explicações da vídeos aula --> https://www.youtube.com/watch?v=FVqzKAUsM68&t=1538s

---
+++ 2021/wp/temas/part-01/facul/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Criando Tema</title>
+</head>
+<body>
+<?php   
+    /*
+    function fucaoum(){
+        echo "<p>PAragrafo dentro do echo!</p>";
+    }
+    fucaoum();
+    fucaoum();
+    */
+
+?>
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p>
+</body>
+</html>
