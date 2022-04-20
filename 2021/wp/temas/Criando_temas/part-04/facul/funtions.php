5bb1452f9649f4e898483df02c9440c8b44190ed
Alan Queirós de Goes <alanqg@gmail.com>
2022-04-20 13:45:45 -0300

    movimentação de pastas

--- 2021/wp/temas/Criando_temas/part-04/facul/funtions.php
+++
@@ -1,9 +0,0 @@
-<?php
-    function script_customer() {
-        //Chamando o estilos css    
-        wp_enqueue_style('customer_main_style', get_stylesheet_uri());
-    }
-     
-    //Chamando os estilos é os scripts 
-    add_action('wp_enqueue_scripts', 'script_customer(');
-?>
