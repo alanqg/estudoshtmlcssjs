d5bf88c98ab3be9b2f0f9562afcd7eb65aac22ec
Alan Queirós de Goes <alanqg@gmail.com>
2022-04-27 14:01:49 -0300

    Tema WP

--- 2021/wp/temas/Criando_temas/01/hospesites/footer.php
+++
@@ -1,7 +0,0 @@
-<footer id="colophon" class="site-footer">
-    <p><?php bloginfo(); ?></p>
-    <?php wp_footer();?>
-</footer>
-
-
-
---
+++ 2021/wp/temas/Criando_temas/01/temawp/index.php
@@ -0,0 +1,8 @@
+<?php 
+    /*Estudos HospeSites*/
+?>
+<?php get_header(); ?>
+
+<p>Principal</p>
+
+<?php get_footer(); ?>
