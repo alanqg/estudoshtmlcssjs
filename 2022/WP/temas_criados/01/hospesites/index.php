d68b4ad1d58f81f14cfa68e51f04d159c255a6f6
Alan Queirós de Goes <alanqg@gmail.com>
2022-07-13 17:32:25 -0300

    fgfgfgf

---
+++ 2022/WP/temas_criados/01/hospesites/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Criando Temas</title>
+</head>
+<body <?php body_class(); ?>>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">Logo</div>
+                <div class="col-md-8">
+                    <nav>
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Sobre</a></li>
+                            <li><a href="#">Sobre</a></li>
+                            <li><a href="#">Contato</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+    <main>
+        <div class="banner">
+            <h2>Primeira caixa de contúdo</h2>
+        </div>
+    </main>
+</body>
+</html>
